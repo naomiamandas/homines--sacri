d2c4b1c3c3f1d95303e84105cdae6c0dc02e3ac4
naomiamanda <naomiamanda@Naomis-MacBook-Pro.local>
2018-12-09 18:52:17 +0100

    newpage

---
+++ script2.js
@@ -0,0 +1,39 @@
+function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
+	var randomX = Math.floor(Math.random()*x);
+	var randomY = Math.floor(Math.random()*y);
+	return [randomX,randomY];
+}
+
+
+window.onload = () => {
+
+function EyePosRandom() {
+  var eye = document.createElement('img');
+	eye.setAttribute("style", "position:absolute; width:13%;  pointer-events: none;");
+	eye.setAttribute("src", "./img/eye.gif");
+	document.body.appendChild(eye);
+	var xy = getRandomPosition(eye);
+	eye.style.top = xy[0] + 'px';
+	eye.style.left = xy[1] + 'px';
+};
+
+for (i = 0; i < 10; i++){
+  EyePosRandom(i);
+};
+
+
+
+
+document.onclick =  function Click() {
+    var eye = document.createElement('img');
+  	eye.setAttribute("style", "position:absolute; width:13%;");
+  	eye.setAttribute("src", "./img/eye.gif");
+  	document.body.appendChild(eye);
+  	var xy = getRandomPosition(eye);
+  	eye.style.top = xy[0] + 'px';
+  	eye.style.left = xy[1] + 'px';
+}
+
+};
